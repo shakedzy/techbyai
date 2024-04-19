c833f1741cb5a83b82ababf41d6fe576f6b155ea
Shaked Zychlinski <shakedzy@gmail.com>
2024-04-20 00:04:26 +0300

    Update item_suggestion.py

--- techbyai/item_suggestion.py
+++ techbyai/item_suggestion.py
@@ -15,10 +15,5 @@ class ItemSuggestion:
     @property
     def id(self) -> str:
         return self.title[0].upper() + str(int(hash(self.url)%99999))
-    
-    @property
-    def alphanumeric_title(self) -> str:
-        t = re.sub(r'[^A-Za-z0-9 ]+', '', self.title)
-        return t.replace(' ', '-').lower()
-    
+
     as_dict = asdict 
