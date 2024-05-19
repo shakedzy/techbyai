4369855c5cd6dceaab167a19a989e775f5d3e24f
Shaked Zychlinski <shakedzy@gmail.com>
2024-05-19 10:03:40 +0300

    Update assistant.py

--- techbyai/assistant.py
+++ techbyai/assistant.py
@@ -58,7 +58,7 @@ class Assistant:
             except Exception as e:
                 error_message = f'ERROR - {e.__class__.__name__}: {e}'
                 if as_json:
-                    error_message = f'{{ "error": {error_message} }}'
+                    error_message = f'{{ "error": "{error_message}" }}'
                     json_error_message = json.loads(error_message)
                 else:
                     json_error_message = {}
